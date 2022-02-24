6164281f1a33c99eb481a14abdbd20870f2ac952
okonri <ikeclemens@gmail.com>
2022-02-24 15:02:30 -0500

    airbnb graphql

---
+++ resolvers.js
@@ -0,0 +1,103 @@
+const User = require('./models/User')
+const Listing = require('./models/Listing')
+const Booking = require('./models/Booking')
+
+exports.resolvers = {
+    Query: {
+        login: async (parent, args) => {
+            let user = await User.findOne({$and: [{username: args.username}, {password: args.password}]})
+            if (!user) {
+                throw new Error("User and Password Didn't match")
+            }
+            if (user.type == 'admin') {
+                return { secret: process.env.SECRET_ADMIN }
+            } else {
+                return { secret: process.env.SECRET_USER }
+            }
+        },
+        
+        getAllUserBooking: async (parent, args) => {
+            let bookings = []
+            if (args.username) {
+                if (args.secret == process.env.SECRET_USER) {
+                    bookings = await Booking.find({username: args.username})
+                } else {
+                    throw new Error("User login required to view bookings")
+                }
+            } else {
+                if (args.secret == process.env.SECRET_ADMIN) {
+                    bookings = await Booking.find({})
+                } else {this
+                    throw new Error("Admin login required to for this section ")
+                }
+            }
+            return bookings
+        },
+
+        getAllAdminListings: async (parent, args) => {
+            if (args.secret == process.env.SECRET_ADMIN) {
+                return await Listing.find({})
+            } else {
+                throw new Error("Admin login required to for this section ")
+            }
+        },
+
+        searchListingByName: async (parent, args) => {
+            return await Listing.find({"listing_title" : args.listing_title})
+        },
+
+        searchListingByCity: async (parent, args) => {
+            return await Listing.find({"city" : args.city})
+        },
+
+        searchListingByPostalCode: async (parent, args) => {
+            return await Listing.find({"postal_code" : args.postal_code})
+        },
+
+    },
+
+    Mutation: {
+        addUser: async (parent, args) => {
+            let user = new User({
+                username: args.username,
+                firstname: args.firstname,
+                lastname: args.lastname,
+                password: args.password,
+                email: args.email
+            })
+            return user.save()
+        },
+        addListing: async (parent, args) => {
+            if (args.secret == process.env.SECRET_ADMIN) {
+                let listing = new Listing({
+                    listing_id: args.listing_id,
+                    listing_title: args.listing_title,
+                    description: args.description,
+                    street: args.street,
+                    city: args.city,
+                    postal_code: args.postal_code,
+                    price: args.price,
+                    email: args.email,
+                    username: args.username,
+                    secret: args.secret
+                })
+                return listing.save()
+            }
+            throw new Error("Authentication Failed, please make sure you include your secret in your request.")
+        },
+        addBooking: async (parent, args) => {
+            if (args.secret == process.env.SECRET_ADMIN) {
+                let booking = new Booking({
+                    listing_id: args.listing_id,
+                    booking_id: args.booking_id,
+                    booking_start: args.booking_start,
+                    booking_end: args.booking_end,
+                    username: args.username,
+                    secret: args.secret
+                })
+                return booking.save()
+            }
+            throw new Error("Authentication Failed, please make sure you include your secret in your request.")
+        }
+    }
+}
---
+++ schema.js
@@ -0,0 +1,86 @@
+const { gql } = require('apollo-server-express');
+
+exports.typeDefs = gql `
+    type Employee {
+        id: ID!
+        username: String!
+        firstname: String!
+        lastname: String!
+        password: String!
+        email: String!
+        type: String!
+    }
+
+    type Listing {
+        id: ID!
+        listing_id: String!
+        listing_title: String!
+        description: String!
+        street: String!
+        city: String!
+        postal_code: String!
+        price: Float!
+        email: String!
+        username: String!
+    }
+
+    type Booking {
+        id: ID!
+        listing_id: String!
+        booking_id: String!
+        booking_date: Date!
+        booking_start: Date!
+        booking_end: Date!
+        username: String!
+    }
+
+    
+    type Query {
+        searchListingByName(listing_title: String!) : [Listing]
+        searchListingByCity(city: String!) : [Listing]
+
+        login(username: String!, password: String!) : Auth
+        getAllUserBooking(username: String, secret: String!) : [Booking]
+        
+        getAllAdminListings(secret: String!) : [Listing]
+
+        searchListingByPostalCode(postal_code: String!) : [Listing]
+    }
+
+    type Auth {
+        secret: String
+    }
+
+    type Mutation {
+        addUser(
+            username: String!
+            firstname: String!
+            lastname: String!
+            password: String!
+            email: String!
+            type: String!
+        ) : User
+        addListing(
+            listing_id: String!
+            listing_title: String!
+            description: String!
+            street: String!
+            city: String!
+            postal_code: String!
+            price: Float!
+            email: String!
+            username: String!
+            secret: String!
+        ) : Listing
+        addBooking(
+            listing_id: String!
+            booking_id: String!
+            booking_start: Date!
+            booking_end: Date!
+            username: String!
+            secret: String!
+        ) : Booking
+    }
+
+
+`
